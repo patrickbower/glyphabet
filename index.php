4d7b4b2c46f29536fb8cf8f49a3880921e961367
Patrick Bower <bower.patrick@gmail.com>
2013-10-23 18:05:51 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="Glyphabet Side Project">
+        <meta name="viewport" content="width=device-width">
+        <link href="assets/css/style.css" media="screen" rel="stylesheet" type="text/css" />
+
+
+    </head>
+    <body>
+
+        <?php include('modules/header.php'); ?>
+        <div class="belt">
+            <?php include('modules/main.php'); ?>
+        </div>
+
+        <script src="assets/js/plugins.js"></script>
+        <script src="assets/js/main.js"></script>
+    </body>
+</html>
